815999571111a871dd00d6cecfa84fa47ff36fe4
Eduardo Silva <eduardo@monkey.io>
2018-03-12 18:31:23 -0700

    tools: mc: new command -I, -i
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- tools/mc.c
+++ tools/mc.c
@@ -24,7 +24,9 @@ static void mc_help(int rc)
 {
     printf("Usage: mc [OPTION]\n\n");
     printf("%sAvailable Options%s\n", ANSI_BOLD, ANSI_RESET);
-    printf("  -i, --init[=PATH]\tinitialize environment\n");
+
+    printf("  -I, --init[=PATH]\tinitialize environment\n");
+    printf("  -i, --info[=PATH]\tinitialize environment\n");
     printf("  -l, --list\t\tlist blocks\n");
     printf("  -b, --block\t\tcreate a new block\n");
     printf("  -h, --help\t\tprint this help\n");
@@ -40,7 +42,8 @@ static int cmd_list_blocks()
     int ret;
     int count;
     char tmp[PATH_MAX];
-    char hash[65];
+    unsigned char hash[32];
+    char str_hash[65];
     struct mk_list *head;
     struct mk_list *list;
     struct mc_block_info *bi;
@@ -59,16 +62,13 @@ static int cmd_list_blocks()
     mk_list_foreach(head, list) {
         bi = mk_list_entry(head, struct mc_block_info, _head);
 
-        /* Hash to Hex */
-        off = 0;
-        for (i = 0; i < 32; i++) {
-            snprintf(hash + off, sizeof(hash) - off, "%02x", bi->block.hash[i]);
-            off += 2;
-        }
-        hash[64] = '\0';
+        /* Calculate Hash */
+        mc_block_hash(&bi->block, (unsigned char *) &hash);
 
-        printf("[blk %08i] %s %s\n",
-               bi->block.block_id, hash, bi->path);
+        /* Hash to Hex */
+        mc_utils_hash_to_string(hash, (char *) &str_hash);
+        mc_info("%s[blk %08i]%s %s %s",
+                ANSI_BOLD, bi->block.block_id, ANSI_RESET, str_hash, bi->path);
     }
 }
 
@@ -89,13 +89,77 @@ static int cmd_block_create()
         return -1;
     }
 
-    block = mc_block_create(tmp);
+    block = mc_block_create(tmp, NULL);
     if (!block) {
         return -1;
     }
     mc_block_save(block, tmp);
 
     free(block);
+    return 0;
+}
+
+/* Given a Hash, retrieve information of the proper block associated */
+static int cmd_info(char *str_hash)
+{
+    int i;
+    int c;
+    int ret;
+    unsigned char hash[32];
+    unsigned char out[32];
+    char out_hash[65];
+    char tmp[PATH_MAX];
+    struct mk_list *list;
+    struct mk_list *head;
+    struct mc_block_info *b;
+
+    /* Get environment absolute path */
+    ret = mc_env_default(tmp, sizeof(tmp) - 1);
+    if (ret != 0) {
+        return -1;
+    }
+
+    list = mc_block_list_create(tmp, &c);
+    if (!list) {
+        mc_error("[hash info] error reading blocks information");
+        return -1;
+    }
+
+    /*
+     * Convert string-hash to binary:
+     *
+     * FIXME: this routine must reverse str_hash to binary hash, for now
+     * I am using the str_hash comparisson which is more expensive.
+     */
+
+    /* Iterate each block search a matching block-header hash */
+    mk_list_foreach(head, list) {
+        b = mk_list_entry(head, struct mc_block_info, _head);
+
+        /* Get the block hash and compare it */
+        mc_block_hash(&b->block, (unsigned char *) &out);
+
+        mc_utils_hash_to_string((unsigned char *) &out, (char *) &out_hash);
+        /* FIXME: str_hash->hash
+        if (memcmp(out, hash, 32) == 0) {
+            break;
+        }
+        */
+        if (strcasecmp(str_hash, out_hash) == 0) {
+            break;
+        }
+        b = NULL;
+    }
+
+    if (!b) {
+        mc_block_list_destroy(list);
+        mc_error("[hash info] no data found associated with %s", str_hash);
+        return -1;
+    }
+
+    mc_block_print_info(b);
+    mc_block_list_destroy(list);
+
     return 0;
 }
 
@@ -106,16 +170,20 @@ int main(int argc, char **argv)
     int optid = 1;
 
     static const struct option long_opts[] = {
-        {"init",  optional_argument, NULL, 'i'},
+        {"init",  optional_argument, NULL, 'I'},
+        {"info",  required_argument, NULL, 'i'},
         {"block", no_argument      , NULL, 'b'},
         {"list",  no_argument      , NULL, 'l'},
         {"help",  no_argument      , NULL, 'h'},
     };
 
-    while ((opt = getopt_long(argc, argv, "i::blh", long_opts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "I::i:blh", long_opts, NULL)) != -1) {
         switch (opt) {
-        case 'i':
+        case 'I':
             ret = mc_create_environment(optarg);
+            return ret;
+        case 'i':
+            ret = cmd_info(optarg);
             return ret;
         case 'b':
             ret = cmd_block_create();
